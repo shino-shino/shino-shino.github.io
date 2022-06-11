76ff4e52cfab40d37cf2293a2229ce97b7241409
shino <106672783+shinotai@users.noreply.github.com>
2022-06-11 20:57:01 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,72 @@
+'use strict'
+
+{
+  function inViewCallback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  function onScrollCallback(entries) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        header.classList.add('scrolled');
+        toTop.classList.add('scrolled');
+      } else {
+        header.classList.remove('scrolled');
+        toTop.classList.remove('scrolled');
+      }
+    });
+  }
+
+  // inView
+  const inViewObserver = new IntersectionObserver(inViewCallback, {
+    threshold: 0.2,
+  });
+
+  document.querySelectorAll('.animate').forEach(el => {
+    inViewObserver.observe(el);
+  });
+
+  // scroll
+  const header = document.querySelector('header');
+  const toTop = document.getElementById('to_top');
+
+  const onScrollObserver = new IntersectionObserver(onScrollCallback);
+  onScrollObserver.observe(document.getElementById('target'));
+
+  toTop.addEventListener('click', e => {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+  // header_navigation
+  window.addEventListener('DOMContentLoaded', () => {
+    const anchorLinks = document.querySelectorAll('a[href^="#"]')
+    const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+  
+    anchorLinksArr.forEach(link => {
+      link.addEventListener('click', e => {
+        e.preventDefault();
+        const targetId = link.hash;
+        const targetElement = document.querySelector(targetId);
+        const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top;
+        
+        window.scrollTo({
+          top: targetOffsetTop,
+          behavior: "smooth"
+        });
+      });
+    });
+  });
+
+}
